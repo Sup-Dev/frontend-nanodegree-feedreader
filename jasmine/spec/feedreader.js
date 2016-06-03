d492ea249354cf367ee4e26213eb990cdabfcbc6
Sup-Dev <sourabh.dev90@gmail.com>
2016-06-03 21:21:04 +0530

    test: all test for initial entries completed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -83,7 +83,9 @@ $(function() {
                 expect($('body').hasClass("menu-hidden")).toBe(true);
             })
     });
+    
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -91,7 +93,21 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+        
+        it('.entry element in .feed container after loadFeed called', function(done) {
+            var entry = $(".feed .entry")[0];
+            //console.log(entry);
+            expect(entry).toBeDefined();
+            expect(entry.length).not.toBe(0);
+            done();
+        });
+    });
+        
     /* TODO: Write a new test suite named "New Feed Selection"
 
         /* TODO: Write a test that ensures when a new feed is loaded
